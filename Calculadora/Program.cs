4ee16572f15817a4734cda88fa7418be8b5c4ff0
Sara Monteiro <saramonteiro2008@gmail.com>
2025-02-04 15:53:27 -0300

    Codigo adição pedindo para o usuário insira o número

---
+++ Calculadora/Program.cs
@@ -0,0 +1,19 @@
+﻿using Calculadora;
+
+Console.Clear();
+
+repetir:
+Console.WriteLine($"Insira o primeiro valor: ");
+if (!decimal.TryParse(Console.ReadLine(), out decimal valor1))
+{
+    Console.WriteLine("Valor inválido! ");
+    goto repetir;
+}
+ Console.WriteLine($"Insira o segundo valor: ");
+ if (!decimal.TryParse(Console.ReadLine(), out decimal valor2))
+{
+     Console.WriteLine("Valor inválido! ");
+     goto repetir;
+}
+
+Operacoes.RealizarAdicao(valor1, valor2);
